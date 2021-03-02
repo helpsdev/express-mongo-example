53738413fae7f594b6489bc0757175c782cf39ad
Edwin Aragon <ared2728@gmail.com>
2021-03-02 08:20:16 -0700

    Add PUT and DELETE for Update and Delete operations

--- app.js
+++ app.js
@@ -9,25 +9,30 @@ const client = new MongoClient(process.env.MONGODB_URL, {
 app.get("/", (req, res) => res.send("Hello world"));
 
 app.get("/collections", (req,res) => {
+    
     client.connect()
         .then(client => {
             client.db("school")
                 .collections()
-                .then(collections => res.json(collections.map(c => c.collectionName)));
+                .then(collections => res.json(collections.map(c => c.collectionName)))
+                .finally(() => client.close());
         })
         .catch(handleConnectionErr);
-    
 });
 
 app.get("/collections/:collectionName", (req, res) => {
-    client
-        .connect()
+    const client = new MongoClient(process.env.MONGODB_URL, {
+        useUnifiedTopology : true
+    });
+    
+    client.connect()
         .then(client => {
             const { collectionName } = req.params;
 
             client.db("school").collection(collectionName, (err, c) => {
                 
-                c.find({}).toArray().then(docs => res.json(docs));
+                c.find({}).toArray().then(docs => res.json(docs))
+                .finally(() => client.close());
 
             });
         }).catch(handleConnectionErr);
@@ -36,6 +41,9 @@ app.get("/collections/:collectionName", (req, res) => {
 
 app.use(express.json());
 app.post("/collections/:collectionName", (req, res) => {
+    const client = new MongoClient(process.env.MONGODB_URL, {
+        useUnifiedTopology : true
+    });
     
     client.connect()
         .then(client => {
@@ -45,12 +53,55 @@ app.post("/collections/:collectionName", (req, res) => {
                 .collection(collectionName, (err, c) => {
                     
                     c.insertOne(req.body)
-                            .then(r => res.json(r.insertedCount));    
+                            .then(r => res.json(r.insertedCount))
+                            .finally(() => client.close());
                     
                 });
         });
-
 });
+
+app.delete("/collections/:collectionName/:studentName", (req, res) => {
+    const client = new MongoClient(process.env.MONGODB_URL, {
+        useUnifiedTopology : true
+    });
+    
+    client.connect()
+        .then(client => {
+            const { collectionName, studentName } = req.params;
+
+            client.db("school")
+                .collection(collectionName, (err, collection) => {
+                    collection
+                        .findOneAndDelete({ name: studentName }, (err, result) => {
+                            res.json(result.value);
+                            client.close();
+                        })
+                })
+        });
+});
+
+app.put('/collections/:collectionName/:studentName', (req, res) => {
+    const client = new MongoClient(process.env.MONGODB_URL, {
+        useUnifiedTopology: true
+    });
+
+    client.connect()
+        .then(client => {
+            const { collectionName, studentName } = req.params;
+            const updateDocument = {
+                $set: { ...req.body }
+            };
+
+            client.db("school")
+                .collection(collectionName, (err, collection) => {
+                    collection
+                        .findOneAndUpdate({ name: studentName }, updateDocument, (err, result) => {
+                            res.json(result.value)
+                            client.close();
+                        })
+                })
+        })
+})
 
 const handleConnectionErr = err => console.log(err);
 
