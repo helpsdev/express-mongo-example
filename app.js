70de7ec1b8ec3ada82b321e8b74aa1c83ccb7d20
Edwin Aragon <ared2728@gmail.com>
2021-03-29 22:13:21 -0700

    Handle session in different routes

--- app.js
+++ app.js
@@ -11,40 +11,65 @@ const session = require('express-session');
 
 passport.use(new BasicStrategy(
     function (username, password, done) {
-        // client.connect()
-        //     .then(client => {
-        //         client.db("school")
-        //             .collection("users", (err, collection) => {
-        //                 collection.findOne({ username: username })
-        //                     .then(user => {
-        //                         if (user.password === password){
-        //                             done(null, user);
-        //                         }else{
-        //                             done(null, false);
-        //                         }
-        //                     }).catch(() => done(null, false));
-        //             });
-        //     });
-        if (username === "edwin" && password === "12345") {
-            done(null, {id:"1", name:"edwin", test:"test1",test32:"teset"});
-        }else{
-            done(null, false);
-        }
+        client.connect()
+            .then(client => {
+                client.db().collection("users", (err, collection) => {
+                        collection.findOne({ username: username })
+                            .then(user => {
+                                if (user.password === password){
+                                    done(null, user);
+                                }else{
+                                    done(null, false);
+                                }
+                            }).catch(() => done(null, false));
+                    });
+            });
+        // if (username === "edwin" && password === "12345") {
+        //     done(null, {id:"1", name:"edwin", test:"test1",test32:"teset"});
+        // }else{
+        //     done(null, false);
+        // }
     }
 ));
 passport.serializeUser(function(user, done) {
-    done(null, user.id);
+    done(null, user._id);
   });
   
   passport.deserializeUser(function(id, done) {
-    done(null,  {id:"1", name:"edwin", test:"test1",test32:"teset"});
+    client.connect()
+        .then(client => {
+            client.db().collection("users", (err, collection) => {
+                collection.findOne({_id: id})
+                    .then(user => {
+                        done(err, user);
+                    }).catch(() => done(null, false));
+            })
+        });
   });
+//not work
+app.get("/logout", session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}), (req, res) => {
+    req.session.destroy(() => {
+        res.clearCookie('connect.sid');
+        res.redirect('/login');
+    });
+})
+//not work
+// app.get("/logout", (req, res) => {
+//     req.logout();
+//     res.redirect('/login');
+// })
 
 app.get("/login", (req, res) => res.send("Send credentials"));
 
 app.post("/login", [
     session({
-        secret: 'keyboard cat'
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: true
     }),
     passport.initialize(),
     passport.session(),
@@ -56,22 +81,24 @@ app.post("/login", [
 
 app.get("/", [
     session({
-        secret: 'keyboard cat'
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: true
     }),
+    passport.initialize(),
+    passport.session(),
+    passport.authenticate('basic'),
     (req, res, next) => {
         console.log(req.session);
         next();
-    },
-    passport.initialize(),
-    passport.session(),
-    passport.authenticate('basic')
+    }
 ], (req, res) => res.send("Hello world"));
 
 app.get("/collections", (req,res) => {
     
     client.connect()
         .then(client => {
-            client.db("school")
+            client.db()
                 .collections()
                 .then(collections => res.json(collections.map(c => c.collectionName)))
                 .finally(() => client.close());
@@ -79,7 +106,9 @@ app.get("/collections", (req,res) => {
         .catch(handleConnectionErr);
 });
 
-app.get("/collections/:collectionName", (req, res) => {
+app.get("/collections/:collectionName", 
+    passport.authenticate('basic', { session: false }),
+    (req, res) => {
     const client = new MongoClient(process.env.MONGODB_URL, {
         useUnifiedTopology : true
     });
@@ -88,7 +117,7 @@ app.get("/collections/:collectionName", (req, res) => {
         .then(client => {
             const { collectionName } = req.params;
 
-            client.db("school").collection(collectionName, (err, c) => {
+            client.db().collection(collectionName, (err, c) => {
                 
                 c.find({}).toArray().then(docs => res.json(docs))
                 .finally(() => client.close());
@@ -110,8 +139,7 @@ app.post("/collections/:collectionName",
             .then(client => {
                 const { collectionName } = req.params;
 
-                client.db("school")
-                    .collection(collectionName, (err, c) => {
+                client.db().collection(collectionName, (err, c) => {
                         
                         c.insertOne(req.body)
                                 .then(r => res.json(r.insertedCount))
@@ -130,8 +158,7 @@ app.delete("/collections/:collectionName/:studentName", (req, res) => {
         .then(client => {
             const { collectionName, studentName } = req.params;
 
-            client.db("school")
-                .collection(collectionName, (err, collection) => {
+            client.db().collection(collectionName, (err, collection) => {
                     collection
                         .findOneAndDelete({ name: studentName }, (err, result) => {
                             res.json(result.value);
@@ -153,8 +180,7 @@ app.put('/collections/:collectionName/:studentName', (req, res) => {
                 $set: { ...req.body }
             };
 
-            client.db("school")
-                .collection(collectionName, (err, collection) => {
+            client.db().collection(collectionName, (err, collection) => {
                     collection
                         .findOneAndUpdate({ name: studentName }, updateDocument, (err, result) => {
                             res.json(result.value)
